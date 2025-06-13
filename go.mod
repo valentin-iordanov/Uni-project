d4b206ea298d89294f26b58ed95736a11fa682ce
valentin-iordanov <72465027+valentin-iordanov@users.noreply.github.com>
2025-06-14 02:31:08 +0300

    Replace project with simple Go flappy bird game

--- go.mod
+++ go.mod
@@ -1,16 +1,8 @@
-module travel
+module flappybird
 
 go 1.21.1
 
 require (
-	github.com/doug-martin/goqu/v9 v9.19.0
-	github.com/go-sql-driver/mysql v1.7.1
-	github.com/golang-migrate/migrate/v4 v4.17.0
-	github.com/gorilla/mux v1.8.1
-)
-
-require (
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
+    github.com/hajimehoshi/ebiten/v2 v2.8.8
+    golang.org/x/image v0.14.0
 )
--- main.go
+++ main.go
@@ -1,109 +1,115 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"travel/internal/handler"
-	"travel/internal/service"
-	"travel/internal/storage"
+    "fmt"
+    "image/color"
+    "math/rand"
+    "time"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/mysql"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+    "github.com/hajimehoshi/ebiten/v2/text"
+    "golang.org/x/image/font/basicfont"
 )
 
+const (
+    screenWidth  = 320
+    screenHeight = 480
+    birdX        = 80
+    birdSize     = 20
+    pipeWidth    = 40
+    pipeGap      = 120
+)
+
+type Pipe struct {
+    x     float64
+    gapY  float64
+    scored bool
+}
+
+type Game struct {
+    birdY   float64
+    birdVel float64
+    pipes   []Pipe
+    score   int
+    ticks   int
+}
+
+func NewGame() *Game {
+    rand.Seed(time.Now().UnixNano())
+    return &Game{birdY: screenHeight / 2}
+}
+
+func (g *Game) Reset() {
+    g.birdY = screenHeight / 2
+    g.birdVel = 0
+    g.pipes = nil
+    g.score = 0
+    g.ticks = 0
+}
+
+func (g *Game) Update() error {
+    if inpututil.IsKeyJustPressed(ebiten.KeySpace) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+        g.birdVel = -6
+    }
+    g.birdVel += 0.4
+    g.birdY += g.birdVel
+
+    if g.ticks%90 == 0 {
+        gapY := 60 + rand.Float64()*(screenHeight-2*60-pipeGap)
+        g.pipes = append(g.pipes, Pipe{x: screenWidth, gapY: gapY})
+    }
+    g.ticks++
+
+    for i := range g.pipes {
+        g.pipes[i].x -= 2
+    }
+
+    for len(g.pipes) > 0 && g.pipes[0].x+pipeWidth < birdX {
+        if !g.pipes[0].scored {
+            g.score++
+        }
+        g.pipes = g.pipes[1:]
+    }
+
+    if g.birdY+birdSize > screenHeight || g.birdY < 0 {
+        g.Reset()
+    }
+
+    for _, p := range g.pipes {
+        if birdX+birdSize > p.x && birdX < p.x+pipeWidth {
+            if g.birdY < p.gapY || g.birdY+birdSize > p.gapY+pipeGap {
+                g.Reset()
+            }
+        }
+    }
+
+    return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+    screen.Fill(color.RGBA{135, 206, 235, 255})
+
+    ebitenutil.DrawRect(screen, birdX, g.birdY, birdSize, birdSize, color.RGBA{255, 255, 0, 255})
+
+    for _, p := range g.pipes {
+        ebitenutil.DrawRect(screen, p.x, 0, pipeWidth, p.gapY, color.RGBA{0, 128, 0, 255})
+        ebitenutil.DrawRect(screen, p.x, p.gapY+pipeGap, pipeWidth, screenHeight-(p.gapY+pipeGap), color.RGBA{0, 128, 0, 255})
+    }
+
+    text.Draw(screen, fmt.Sprintf("Score: %d", g.score), basicfont.Face7x13, 10, 20, color.Black)
+}
+
+func (g *Game) Layout(outW, outH int) (int, int) {
+    return screenWidth, screenHeight
+}
+
 func main() {
-
-	dbName := "travel"
-
-	//create db connection
-	db, err := createDatabase(dbName)
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-
-	//create db migration
-	m, err := setUpMigrations(db, dbName)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
-	}
-
-	//create storage
-	storage := storage.New(db, "mysql")
-
-	//create service
-	service := service.New(storage)
-
-	//create handler
-	handler := handler.New(service)
-
-	srv := http.Server{
-		Addr:    ":8080",
-		Handler: handler,
-	}
-
-	log.Println("I am running ... localhost:8080")
-
-	if err := srv.ListenAndServe(); err != nil {
-		panic(err)
-	}
+    ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+    ebiten.SetWindowTitle("Flappy Bird")
+    if err := ebiten.RunGame(NewGame()); err != nil {
+        panic(err)
+    }
 }
 
-func createDatabase(dbName string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:root@tcp(db:3306)/?parseTime=true&multiStatements=true")
-
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-
-	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
-	if err != nil {
-		return nil, err
-	}
-
-	db.Close()
-
-	db, err = sql.Open("mysql", "root:root@tcp(db:3306)/"+dbName+"?parseTime=true&multiStatements=true")
-
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func setUpMigrations(db *sql.DB, dbName string) (*migrate.Migrate, error) {
-	driver, err := mysql.WithInstance(db, &mysql.Config{DatabaseName: dbName})
-	if err != nil {
-		return nil, err
-	}
-
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://migrations",
-		"mysql",
-		driver,
-	)
-	if err != nil {
-		fmt.Println("errorr")
-		return nil, err
-	}
-
-	return m, nil
-}
